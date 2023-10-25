9d0345bc4775c082d8f2f6e77437225dfeda6557
정소정 <thwjd0205@naver.com>
2023-10-25 11:00:40 +0900

    init: storybook
    Change-Id: I5324d2171c866aa2c087a0ec9473022fecee22d5

--- Frontend/BeNurse/.eslintrc.cjs
+++ Frontend/BeNurse/.eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended"
   ],
   overrides: [
     {
