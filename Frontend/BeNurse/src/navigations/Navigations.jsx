2dc07282ddc0c0ce36341d8eba9cb207230506d9
정소정 <thwjd0205@naver.com>
2023-10-26 11:33:49 +0900

    feat : pages + route 설정
    Change-Id: I3ea1647c255ead953350f97684fe4f930f629414

--- Frontend/BeNurse/src/navigations/Navigations.jsx
+++ Frontend/BeNurse/src/navigations/Navigations.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import MainPage from "@pages/MainPage";
-import LoginPage from "@pages/LoginPage";
-import DevicePage from "@pages/DevicePage";
+// Page 정리
+import MainPage from "@pages/MainPage/MainPage";
+import LoginPage from "@pages/LoginPage/LoginPage";
+import DevicePage from "@pages//DevicePage/DevicePage";
+import PatientPage from "@pages/PatientPage/PatientPage";
+import HandOverPage from "@pages/HandOverPage/HandOverPage";
+import SchedulePage from "@pages/SchedulePage/SchedulePage";
+import MyPage from "@pages/MyPage/MyPage";
+import NoticePage from "@pages/NoticePage/NoticePage";
 
 export default function routes() {
   return (
@@ -19,6 +25,26 @@ export default function routes() {
       <Route
         path="/device"
         element={<DevicePage />}
+      />
+      <Route
+        path="/patient"
+        element={<PatientPage />}
+      />
+      <Route
+        path="/handover"
+        element={<HandOverPage />}
+      />
+      <Route
+        path="/schedule"
+        element={<SchedulePage />}
+      />
+      <Route
+        path="/mypage"
+        element={<MyPage />}
+      />
+      <Route
+        path="/notice"
+        element={<NoticePage />}
       />
     </Routes>
   );
---
+++ Frontend/BeNurse/src/pages/NoticePage/NoticePage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NoticePage() {
+  return <div>NoticePage</div>;
+}
