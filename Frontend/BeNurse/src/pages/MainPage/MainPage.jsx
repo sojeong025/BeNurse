6c29bb82401b4268dc5ad425d316f662c0fe22d3
정은경 <f1rstf1y9@naver.com>
2023-10-26 14:55:34 +0900

    design: add margin props to button
    Change-Id: I5a0fc181f14200ee66c2b156ab8b7d6fb0354aa2

--- Frontend/BeNurse/src/pages/MainPage/MainPage.jsx
+++ Frontend/BeNurse/src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,13 @@
+import Button from "../../components/atoms/Button/Button";
+
 function MainPage() {
-  return <div>메인 페이지</div>;
+  return (
+    <div>
+      메인 페이지
+      <Button margin="5px 10px 100px 10px">ㅎㅇ</Button>
+      <Button variant="primary">ㅎㅇ</Button>
+    </div>
+  );
 }
 
 export default MainPage;
