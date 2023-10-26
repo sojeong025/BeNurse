6775e08a8e57b1951278547db8c0f35f6fc49ead
정소정 <thwjd0205@naver.com>
2023-10-26 16:03:36 +0900

    feat : input onfocus
    Change-Id: Ia1bb9f96053fba992d0971764e67340cd56fd470

--- Frontend/BeNurse/src/components/atoms/Input/Input.stories.jsx
+++ Frontend/BeNurse/src/components/atoms/Input/Input.stories.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Input from "./Input";
 import { withKnobs } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
 
 export default {
   title: "Components/Input",
@@ -8,5 +9,20 @@ export default {
   decorators: [withKnobs],
 };
 
-export const Default = () => <Input variant="default">Default</Input>;
-export const Search = () => <Input variant="search">Search</Input>;
+export const Default = () => (
+  <Input
+    variant="default"
+    onFocus={action("onFocus")}
+  >
+    Default
+  </Input>
+);
+
+export const Search = () => (
+  <Input
+    variant="search"
+    onFocus={action("onFocus")}
+  >
+    Search
+  </Input>
+);
