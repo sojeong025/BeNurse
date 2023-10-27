d2fa8ef3e7cb1e3c364f84fc7e88595546680481
이종윤 <qjrm69@gmail.com>
2023-10-27 15:47:27 +0900

    fix : Nurse Email field add
    Change-Id: I09aef845a4707f4d81a4636248d130575e4a58b8

--- Backend/BeNurse/src/main/java/com/ssafy/nurse/service/NurseRepository.java
+++ Backend/BeNurse/src/main/java/com/ssafy/nurse/service/NurseRepository.java
@@ -3,10 +3,11 @@ package com.ssafy.nurse.service;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ssafy.nurse.model.Nurse;
 
+@Repository
 public interface NurseRepository extends JpaRepository<Nurse, Long> {
-
 	Optional<Nurse> findByEmail(String email);
 }
