30fedefd8c04456de63f3395d5bf185a67b117d9
김대웅 <kdu201@naver.com>
2023-10-27 15:03:13 +0900

    feat: modify NavBar BG Color

--- Frontend/BeNurse/src/components/templates/NavBar/NavBar.jsx
+++ Frontend/BeNurse/src/components/templates/NavBar/NavBar.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Common } from "../../../utils/global.styles";
 
 export default function NavBar() {
+  const [navColor, setNavColor] = useState(Common.color.white01);
+  const [navFontColor, setNavFontColor] = useState(Common.color.black03);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/schedule") {
+      setNavColor(Common.color.purple03);
+      setNavFontColor(Common.color.white01);
+    } else {
+      setNavColor(Common.color.white01);
+      setNavFontColor(Common.color.black03);
+    }
+  }, [location]);
   return (
     <div
       style={{
@@ -13,7 +28,8 @@ export default function NavBar() {
         width: "412px",
         height: "64px",
         padding: "0px 0px 10px 0px",
-        backgroundColor: "#fff",
+        color: navFontColor,
+        backgroundColor: navColor,
       }}
     >
       <span>장비 관리</span>
