cd435e5fdacc27a479c8238d4c37f52b774ef486
성제현 <sungjae0512@gmail.com>
2023-10-27 15:47:40 +0900

    feat : add
    Change-Id: I5e841bdea55b197b891fc25319edc95a0d7d2039

--- Backend/BeNurse/src/main/java/com/ssafy/Handover/service/HandoverRepository.java
+++ Backend/BeNurse/src/main/java/com/ssafy/Handover/service/HandoverRepository.java
@@ -1,9 +1,11 @@
 package com.ssafy.Handover.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ssafy.Handover.model.Handover;
 
+@Repository
 public interface HandoverRepository extends JpaRepository<Handover, Long> {
 
 }
--- Backend/BeNurse/src/main/java/com/ssafy/hospital/service/HospitalRepository.java
+++ Backend/BeNurse/src/main/java/com/ssafy/hospital/service/HospitalRepository.java
@@ -1,9 +1,11 @@
 package com.ssafy.hospital.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ssafy.hospital.model.Hospital;
 
+@Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
 
 }
