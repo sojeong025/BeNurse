f2fd91b8d3cc5f041e5de9a5ec35f6381597e960
이종윤 <qjrm69@gmail.com>
2023-10-27 16:05:30 +0900

    fix : swagger security 2
    Change-Id: I20f7d31114dd4f6a0d401c7f2d43e794bde1e345

--- Backend/BeNurse/src/main/java/com/ssafy/common/config/SwaggerConfig.java
+++ Backend/BeNurse/src/main/java/com/ssafy/common/config/SwaggerConfig.java
@@ -1,11 +1,20 @@
 package com.ssafy.common.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -21,6 +30,33 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.
                         basePackage("com.ssafy"))
                 .paths(PathSelectors.any())
+                .build();
+    }
+ // 추가
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+    
+    // 추가
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+ 
+    // 추가
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+ 
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Backend API")
+                .description("Backend API 문서")
+                .version("1.0")
                 .build();
     }
 }
