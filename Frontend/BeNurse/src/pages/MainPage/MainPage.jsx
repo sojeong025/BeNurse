2d68c21ce6faa731510b4a0532f359ab51c12327
정은경 <f1rstf1y9@naver.com>
2023-10-30 00:37:50 +0900

    feat: create MainPage
    Change-Id: Ia388643358e50f672184b1307a2f6fc98a8777bf

--- Frontend/BeNurse/src/pages/MainPage/MainPage.jsx
+++ Frontend/BeNurse/src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,131 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineNotification } from "react-icons/ai";
+import { AiOutlineFileText } from "react-icons/ai";
+import Container from "../../components/atoms/Container/Container";
 import Button from "../../components/atoms/Button/Button";
+import Box from "../../components/atoms/Box/Box";
+
+import * as S from "./MainPage.styles";
+import main_nurse from "@assets/Images/main_nurse.png";
+import { ButtonContainer } from "../LoginPage/JoinPage.styles";
+
+import moment from "moment";
 
 function MainPage() {
+  const dates = [];
+  const types = ["day", "night", "evening", "off"];
+  for (let i = 0; i < 7; i++) {
+    const date = moment().add(i, "days");
+    const dayOfWeek = date.format("ddd").toUpperCase();
+    const randomType = types[Math.floor(Math.random() * types.length)]; // 랜덤으로 타입 선택
+    dates.push({ date, dayOfWeek, type: randomType });
+  }
+
   return (
-    <div>
-      메인 페이지
-      <Button margin="5px 10px 100px 10px">ㅎㅇ</Button>
-      <Button variant="primary">ㅎㅇ</Button>
-    </div>
+    <Container>
+      <S.MainContainer>
+        <S.TopContainer>
+          <S.BtnContainer>
+            <Button
+              width="100%"
+              height="80px"
+            >
+              <S.ButtonContent>
+                <AiOutlineUser />
+                <div>마이페이지</div>
+              </S.ButtonContent>
+            </Button>
+            <Button
+              variant="primary"
+              width="100%"
+              height="80px"
+            >
+              <S.ButtonContent>
+                <AiOutlineNotification />
+                <div>공지사항</div>
+              </S.ButtonContent>
+            </Button>
+          </S.BtnContainer>
+
+          <img
+            src={main_nurse}
+            alt="main_nurse"
+            width="200px"
+          />
+        </S.TopContainer>
+        <hr />
+        <div>
+          <S.MainTitle>
+            <p>주간 스케쥴</p>
+          </S.MainTitle>
+          <Box
+            type="white"
+            size={["100%", "140px"]}
+            margin="0 0 30px 0"
+          >
+            <S.scheduleDayBox>
+              {dates.map((item, index) => (
+                <S.scheduleDay
+                  key={index}
+                  type={item.type}
+                >
+                  <p className="day">{item.dayOfWeek}</p>
+                  <p className="date">{item.date.date()}</p>
+                  <div className="type">
+                    {item.type.charAt(0).toUpperCase()}
+                  </div>
+                </S.scheduleDay>
+              ))}
+            </S.scheduleDayBox>
+          </Box>
+        </div>
+        <hr />
+        <S.MainTitle>
+          <p>간호 tip</p>
+        </S.MainTitle>
+
+        <S.TipBoxContainer>
+          <Box
+            type="white"
+            size={["130px", "130px"]}
+          >
+            <S.TipBox>
+              <AiOutlineFileText />
+              <div>
+                <p className="title">임상 실무팁</p>
+                <p className="desc">출혈 - 내출혈 케이스</p>
+              </div>
+            </S.TipBox>
+          </Box>
+          <Box
+            type="white"
+            size={["130px", "130px"]}
+          >
+            <S.TipBox>
+              <AiOutlineFileText />
+              <div>
+                <p className="title">임상 실무팁</p>
+                <p className="desc">출혈 - 내출혈 케이스</p>
+              </div>
+            </S.TipBox>
+          </Box>
+          <Box
+            type="white"
+            size={["130px", "130px"]}
+          >
+            <S.TipBox>
+              <AiOutlineFileText />
+              <div>
+                <p className="title">임상 실무팁</p>
+                <p className="desc">출혈 - 내출혈 케이스</p>
+              </div>
+            </S.TipBox>
+          </Box>
+        </S.TipBoxContainer>
+      </S.MainContainer>
+    </Container>
   );
 }
 
---
+++ Frontend/BeNurse/src/pages/MainPage/MainPage.styles.jsx
@@ -0,0 +1,131 @@
+import styled from "@emotion/styled";
+import { Common } from "@utils/global.styles.jsx";
+
+export const MainContainer = styled.div`
+  width: calc(100% - 28px);
+  height: 100%;
+  margin-top: 130px;
+  background-color: ${Common.color.purple00};
+  border-top-left-radius: 250px;
+  padding: 0 14px;
+`;
+
+export const TopContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: -70px;
+  justify-content: space-evenly;
+`;
+
+export const BtnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 110px;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 15px;
+  padding: 20px 0;
+`;
+
+export const ButtonContent = styled.div`
+  & > svg {
+    width: 1.5em;
+    height: 1.5em;
+  }
+  & > div {
+    font-size: ${Common.fontSize.fontS};
+    font-weight: ${Common.fontWeight.bold};
+  }
+`;
+
+export const MainTitle = styled.div`
+  margin: 16px 0;
+  font-size: ${Common.fontSize.fontM};
+  font-weight: ${Common.fontWeight.extrabold};
+  color: ${Common.color.black03};
+`;
+
+export const scheduleDayBox = styled.div`
+  display: flex;
+  gap: 8px;
+  height: 100%;
+  padding: 14px 0;
+  box-sizing: border-box;
+`;
+
+export const scheduleDay = styled.div`
+  height: 100%;
+  color: ${Common.color.black03};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  & .day {
+    font-size: ${Common.fontSize.fontXS};
+    font-weight: ${Common.fontWeight.extrabold};
+  }
+  & .date {
+    font-size: ${Common.fontSize.fontXS};
+    font-weight: ${Common.fontWeight.bold};
+  }
+  & .type {
+    width: 43px;
+    height: 62px;
+    color: ${Common.color.white01};
+    font-size: ${Common.fontSize.fontXS};
+    font-weight: ${Common.fontWeight.extrabold};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 20px;
+    background-color: ${({ type }) => {
+      if (type === "day") {
+        return Common.color.day;
+      } else if (type === "night") {
+        return Common.color.night;
+      } else if (type === "evening") {
+        return Common.color.evening;
+      } else {
+        return Common.color.off;
+      }
+    }};
+  }
+`;
+
+export const TipBoxContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  gap: 23px;
+  box-sizing: border-box;
+  & > div {
+    flex: 0 0 auto;
+  }
+  padding-bottom: 15px;
+  padding-right: 15px;
+`;
+
+export const TipBox = styled.div`
+  height: 100%;
+  width:100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 15px;
+  gap: 10px;
+  justify-content: space-around;
+  & svg {
+    width: 1.6em;
+    height: 1.6em;
+    color: ${Common.color.purple03};
+  }
+  & .title {
+    font-weight: ${Common.fontWeight.bold};
+    font-size: ${Common.fontSize.fontM};
+    margin-bottom: 8px;
+  }
+  & .desc {
+    font-weight: ${Common.fontWeight.regular};
+    font-size: ${Common.fontSize.fontS};
+  }
+`;
