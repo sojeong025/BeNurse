3c4085d0c77d9f724d9fc384a6b43946aa07a2d6
정소정 <thwjd0205@naver.com>
2023-10-31 17:19:34 +0900

    feat: handoverPage
    Change-Id: I474c457ff217f1a331aee9ca93a50080b7909071

--- Frontend/BeNurse/src/pages/HandOverPage/HandOverPage.styles.jsx
+++ Frontend/BeNurse/src/pages/HandOverPage/HandOverPage.styles.jsx
@@ -1,2 +1,12 @@
 import styled from "@emotion/styled";
 import { Common } from "@utils/global.styles.jsx";
+
+export const NurseHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 60px;
+`;
+
+// export const HandoverList = styled.div`
+//   display: flex;
+// `;
