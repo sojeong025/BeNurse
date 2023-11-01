48ef1282379d6d92e51e22cdc3844ba464ca7163
성제현 <sungjae0512@gmail.com>
2023-11-01 11:23:16 +0900

    fix : add EnableSwagger2 annotation
    Change-Id: Idd3ee83868f0fa255e10e2dcd2ae84a6b038dc6f

--- Backend/BeNurse/src/main/java/com/ssafy/BeNurseApplication.java
+++ Backend/BeNurse/src/main/java/com/ssafy/BeNurseApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 @EnableFeignClients
 @EnableCaching
+@EnableSwagger2
 public class BeNurseApplication {
 
 	public static void main(String[] args) {
