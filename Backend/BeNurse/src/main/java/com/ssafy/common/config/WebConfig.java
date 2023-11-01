fd400c1dec01c0516573e16d4fd9f5316d71755f
성제현 <sungjae0512@gmail.com>
2023-11-01 13:53:26 +0900

    fix : error
    Change-Id: Ic7a0abe1eefb4736ca6d4b76b08d2a4f0ec704f3

--- Backend/BeNurse/src/main/java/com/ssafy/common/config/WebConfig.java
+++ Backend/BeNurse/src/main/java/com/ssafy/common/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("v2/api-docs")
-		.allowedOrigins("http://k9e105.p.ssafy.io:9000")
+		.allowedOrigins("*")
+
 		.allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 }
