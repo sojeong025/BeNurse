25dd325bda84d7eb00c35bf7a3b269f79c7ad6a6
정은경 <f1rstf1y9@naver.com>
2023-11-05 19:25:13 +0900

    fix: HandOverWrite page top margin
    Change-Id: Id236277a6de37523e8c0008d2d82c23a0a858789

--- Frontend/BeNurse/src/pages/HandOverPage/HandOverWritePage.jsx
+++ Frontend/BeNurse/src/pages/HandOverPage/HandOverWritePage.jsx
@@ -58,7 +58,7 @@ export default function HandOverWritePage() {
       <div
         style={{
           marginTop: "30px",
-          paddingTop: "24px",
+          paddingTop: "74px",
           width: "calc(100% - 28px)",
         }}
       >
