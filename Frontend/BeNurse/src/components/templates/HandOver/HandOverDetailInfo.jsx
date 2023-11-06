92c6b11f0d788e1ec9ccc08ddba4343b795ad84a
정은경 <f1rstf1y9@naver.com>
2023-11-07 00:17:28 +0900

    feat: create handover temp patient
    Change-Id: Ifd4f9d176feb9b3965fa02b7351fa33ac0512264

--- Frontend/BeNurse/src/components/templates/HandOver/HandOverDetailInfo.jsx
+++ Frontend/BeNurse/src/components/templates/HandOver/HandOverDetailInfo.jsx
@@ -7,11 +7,27 @@ import {
   PatientDetailItemContainer,
 } from "@pages/PatientPage/PatientDetail.styles.jsx";
 
-
 export default function HandOverDetailInfo() {
+  const tempPatient = {
+    id: "13",
+    age: "25",
+    discharge: "",
+    disease: "다리 외상",
+    drinking: false,
+    gender: "여",
+    hospitalization: "2023-11-07",
+    history: "당뇨",
+    medicine: "",
+    name: "김이박",
+    selfmedicine: "타이레놀",
+    smoking: true,
+    alergy: "마늘",
+    surgery: "다리 수술",
+    cc: [{ content: "아파요" }],
+  };
   return (
     <div style={{ width: "calc(100% - 28px)", margin: "0 auto" }}>
-      <PatientDetailProfile />
+      <PatientDetailProfile patient={tempPatient} />
       <PatientDetailContainer
         style={{
           overflowY: "scroll",
