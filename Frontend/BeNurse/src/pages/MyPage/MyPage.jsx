4521824fe26b36a0df74a3c25355105cf4043cff
정은경 <f1rstf1y9@naver.com>
2023-11-10 10:46:05 +0900

    feat: mypage info, mypage buttons onClick function
    Change-Id: Iad9157dc8ae46fc36e7a3a958c55aa364cf2e99f

--- Frontend/BeNurse/src/pages/MyPage/MyPage.jsx
+++ Frontend/BeNurse/src/pages/MyPage/MyPage.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import { AiOutlinePhone } from "react-icons/ai";
@@ -7,13 +7,96 @@ import { MdSettings } from "react-icons/md";
 import { MdOutlinePrivacyTip } from "react-icons/md";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
-import Container from "../../components/atoms/Container/Container";
+import Container from "@components/atoms/Container/Container";
+import Modal from "@components/atoms/Modal/Modal";
+import Button from "@components/atoms/Button/Button";
+
+import { customAxios } from "../../libs/axios";
 
 import * as S from "./MyPage.styles";
 import change_hospital from "@assets/Images/change_hospital.png";
-import patient_temp from "@assets/Images/patient_temp.png";
+import nurse_woman from "@assets/Images/nurse_woman.png";
+import warning from "@assets/Images/warning.png";
 
 export default function MyPage() {
+  const navigate = useNavigate();
+
+  const [myInfo, setMyInfo] = useState({});
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalItem, setModalItem] = useState({
+    msg: "",
+    button: "",
+    onClick: "",
+  });
+
+  const handleOpenModal = (event, type) => {
+    event.preventDefault();
+    const change_hospital = {
+      msg: (
+        <div>
+          병원 정보를 변경하기 위해서 <br /> 새로 발급받은 인증코드가
+          필요합니다. <br />
+          계속하시겠습니까 ?
+        </div>
+      ),
+      button: "변경하기",
+      onClick: () => {
+        navigate("/login/joinNurse");
+      },
+    };
+    const leave_hospital = {
+      msg: (
+        <div>
+          탈퇴 후 재가입을 위해서 <br />
+          병원 측 인증코드 재발급이 필요합니다. <br />
+          계속 하시겠습니까?
+        </div>
+      ),
+      button: "탈퇴하기",
+      onClick: () => {
+        customAxios
+          .delete("nurse", { id: localStorage.getItem("nurseID") })
+          .then((res) => {
+            console.log("간호사 탈퇴 성공", res);
+          })
+          .catch((error) => {
+            console.error("간호사 탈퇴 실패:", error);
+          });
+        navigate("/login");
+      },
+    };
+    setModalIsOpen(true);
+    if (type == "change_hospital") {
+      setModalItem(change_hospital);
+    } else if (type == "leave_hospital") {
+      setModalItem(leave_hospital);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+    setModalItem({
+      msg: "",
+      button: "",
+      onClick: () => {},
+    });
+  };
+
+  const handleChangeHospitalBtn = () => {};
+
+  useEffect(() => {
+    customAxios
+      .get("nurse/me")
+      .then((res) => {
+        console.log("내 정보 불러오기", res.data.responseData);
+        setMyInfo(res.data.responseData);
+      })
+      .catch((error) => {
+        console.error("내 정보 로드 실패:", error);
+      });
+  }, []);
+
   return (
     <Container>
       <S.MainContainer>
@@ -21,17 +104,17 @@ export default function MyPage() {
           <div className="top-container-inner">
             <img
               className="profile_image"
-              src={patient_temp}
+              src={nurse_woman}
             />
-            <p className="profile_name">정소정</p>
+            <p className="profile_name">{myInfo.name}</p>
             <div className="profile_workplace">
-              <span className="profile_hospital">갑을녹산병원</span>
-              <span className="profile_ward">내과 5동</span>
+              <span className="profile_hospital">{myInfo.hospitalName}</span>
+              <span className="profile_ward">{myInfo.wardName}</span>
             </div>
           </div>
         </S.TopContainer>
         <div>
-          <Link>
+          <div onClick={(e) => handleOpenModal(e, "change_hospital")}>
             <S.ChangeHospitalBtn>
               <img
                 src={change_hospital}
@@ -41,7 +124,7 @@ export default function MyPage() {
                 <p>병원 변경하기</p>
                 <p>이직하셨나요? 병원을 재등록 해주세요.</p>
               </div>
-              <div 
+              <div
                 className="arrow-chip"
                 style={{
                   background: "#D0BFFF",
@@ -54,7 +137,7 @@ export default function MyPage() {
                 <MdKeyboardArrowRight />
               </div>
             </S.ChangeHospitalBtn>
-          </Link>
+          </div>
         </div>
         <S.MyPageList>
           <Link>
@@ -93,10 +176,76 @@ export default function MyPage() {
           </Link>
         </S.MyPageList>
         <S.BtnContainer>
-          <S.MyPageAuthBtn>로그아웃</S.MyPageAuthBtn>
-          <S.MyPageAuthBtn>회원탈퇴</S.MyPageAuthBtn>
+          <S.MyPageAuthBtn
+            onClick={() => {
+              localStorage.removeItem("accessToken");
+              navigate("/login");
+            }}
+          >
+            로그아웃
+          </S.MyPageAuthBtn>
+          <S.MyPageAuthBtn
+            onClick={(e) => {
+              handleOpenModal(e, "leave_hospital");
+            }}
+          >
+            회원탈퇴
+          </S.MyPageAuthBtn>
         </S.BtnContainer>
       </S.MainContainer>
+      <Modal
+        visible={modalIsOpen}
+        closable={false}
+        maskClosable={true}
+        onClose={handleCloseModal}
+        width="300px"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            overflowX: "hidden",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "16px",
+              maxHeight: "600px",
+              overflowY: "auto",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "20px",
+            }}
+          >
+            <img
+              src={warning}
+              style={{ width: "60px" }}
+              alt=""
+            />
+            <div
+              style={{
+                textAlign: "center",
+                lineHeight: "20px",
+                fontSize: "14px",
+              }}
+            >
+              {modalItem.msg}
+            </div>
+            <Button
+              variant="danger"
+              width="100px"
+              height="40px"
+              onClick={modalItem.onClick}
+            >
+              {modalItem.button}
+            </Button>
+          </div>
+        </div>
+      </Modal>
     </Container>
   );
 }
