bdaecf4af5d30cb350035480bbb20cfe63fdd212
정은경 <f1rstf1y9@naver.com>
2023-11-13 12:41:28 +0900

    feat: add update nurse info
    Change-Id: I6b11ef6fda7e660c9fa14d985565ecf738deff49

---
+++ Frontend/BeNurse/src/components/templates/Admin/EmployeeItem.styles.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import { Common } from "../../../utils/global.styles";
+
+export const SelectContainer = styled.div`
+  & .ant-select-selection-item {
+    color: #333333 !important;
+    font-size: 12px !important;
+  }
+  & .ant-select-selector,
+  & .ant-select-item-option-content {
+    font-family: "NanumSquareNeo";
+  }
+`;
